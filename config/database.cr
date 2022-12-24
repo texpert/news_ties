0e67c21f9a4480de448fabd8b2df14d38325b6e2
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2022-12-24 03:27:49 +0200

    Try to fix DB username and pass for the CI

--- config/database.cr
+++ config/database.cr
@@ -1,19 +1,22 @@
 database_name = "news_ties_#{LuckyEnv.environment}"
 
 AppDatabase.configure do |settings|
-  if LuckyEnv.production?
-    settings.credentials = Avram::Credentials.parse(ENV["DATABASE_URL"])
-  else
-    settings.credentials = Avram::Credentials.parse?(ENV["DATABASE_URL"]?) || Avram::Credentials.new(
-      database: database_name,
-      hostname: ENV["DB_HOST"]? || "localhost",
-      port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
-      # Some common usernames are "postgres", "root", or your system username (run 'whoami')
-      username: ENV["DB_USERNAME"]? || "lucky_dev",
-      # Some Postgres installations require no password. Use "" if that is the case.
-      password: ENV["DB_PASSWORD"]? || "lucky_dev_pass"
-    )
+  if db_url = ENV["DATABASE_URL"]?
+    settings.credentials = Avram::Credentials.parse(db_url)
+    next
   end
+
+  next if LuckyEnv.production?
+
+  settings.credentials = Avram::Credentials.new(
+    database: database_name,
+    hostname: ENV["DB_HOST"]? || "localhost",
+    port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
+    # Some common usernames are "postgres", "root", or your system username (run 'whoami')
+    username: ENV["DB_USERNAME"]? || (LuckyEnv.development? ? "lucky_dev" : "postgres"),
+    # Some Postgres installations require no password. Use "" if that is the case.
+    password: ENV["DB_PASSWORD"]? || (LuckyEnv.development? ? "lucky_dev_pass" : "postgres")
+  )
 end
 
 Avram.configure do |settings|
